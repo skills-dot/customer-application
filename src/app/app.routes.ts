a0c46dc372a6f780d30b3d622dc62777e8daeb79
Rambabu Mallavarapu <Rambabu.Mallavarapu@in.ey.com>
2024-12-20 12:41:38 +0530

    Added menu link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'customer-component', component: CustomerComponent },];
+
